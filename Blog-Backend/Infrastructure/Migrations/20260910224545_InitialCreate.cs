dbc0b75ff0fef05f2e7dbbff793accca1df02897
Egor <arnoldtagirov1@gmail.com>
2026-09-11 00:46:22 +0200

    Added Migration

---
+++ Blog-Backend/Infrastructure/Migrations/20260910224545_InitialCreate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ThumbnailUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastActivity = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- Blog-Backend/Infrastructure/Persistence/DbContext/BlogDesignTimeDbFactory.cs
+++ Blog-Backend/Infrastructure/Persistence/DbContext/BlogDesignTimeDbFactory.cs
@@ -1,28 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Options;
 
 namespace Infrastructure.Persistence.DbContexts
 {
-    public class BlogDesignTimeDbFactory:IDesignTimeDbContextFactory<BlogDbContext>
+    public class BlogDesignTimeDbFactory : IDesignTimeDbContextFactory<BlogDbContext>
     {
         public BlogDbContext CreateDbContext(string[] args)
         {
             var configuration = new ConfigurationBuilder()
-                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../BlogUI"))
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../Presentation"))
                 .AddJsonFile("appsettings.json", optional: false)
                 .AddJsonFile("appsettings.Development.json", optional: true)
                 .Build();
-            var connectionString = configuration.GetConnectionString("DefaultConnection")
-    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            var optionsBuilder = new DbContextOptionsBuilder<BlogDbContext>();
-            optionsBuilder.UseSqlServer(connectionString);
-            return new BlogDbContext(optionsBuilder.Options);
 
+            var connectionString = configuration.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException(
+                    "Connection string 'DefaultConnection' not found.");
+
+            var optionsBuilder = new DbContextOptionsBuilder<BlogDbContext>();
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new BlogDbContext(optionsBuilder.Options);
         }
     }
 }
