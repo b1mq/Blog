8a0fd9665c38b6779ff5946548df6680a80c7f21
Egor <arnoldtagirov1@gmail.com>
2026-09-10 15:47:35 +0200

    Added Repository Interface Input Output/Read Write Operations

---
+++ Blog-Backend/Domain/Interfaces/IUserReadRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Entities;
+using Domain.Enums;
+namespace Domain.Interfaces
+{
+    public interface IUserReadRepository
+    {
+        Task<User> GetUserAsync(Guid id);
+        Task<IReadOnlyList<User>> GetUsersAsync(string name);
+        Task<User> GetUserByEmailAsync(string email);
+        Task<IReadOnlyList<User>> GetCreatedUsersByTimeAsync(DateTime createdAt);
+        Task<IReadOnlyList<User>> GetUsersByCountryAsync(string country);
+        Task<IReadOnlyList<User>> GetUsersByRoleAsync(Roles role);
+
+
+    }
+}
---
+++ Blog-Backend/Domain/Interfaces/IUserWriteRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IUserWriteRepository
+    {
+        Task<User> GetUserAsync(Guid id);
+        Task AddUserAsync(User user);
+        Task EditUserAsync(User user);
+        Task RemoveUserAsync(User user);
+        Task RemoveUserByIdAsync(Guid id);
+    }
+}
