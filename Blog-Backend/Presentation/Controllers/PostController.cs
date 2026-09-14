1567fb14b246281f7a34bd3c23fb99ee71246c5c
Egor <arnoldtagirov1@gmail.com>
2026-09-14 23:26:23 +0200

    Added controller for postservice

---
+++ Blog-Backend/Presentation/Controllers/PostController.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Claims;
+using Application.Dtos.Post.AddDtos;
+using Application.Interfaces.PostServiceInterface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+        // доделать контроллер 
+    {
+        private protected IPostService _postService;
+        public PostController(IPostService postService)
+        {
+            _postService = postService;
+        }
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> AddNewPost([FromBody] AddPostDto postDto)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if(userId == null)
+            {
+                return Unauthorized();
+            }
+            var result = await _postService.AddNewPost(postDto,Guid.Parse(userId));
+            if(!result.isSucces)
+            {
+                return BadRequest(result.Error);
+            }
+            return Ok();
+
+        }
+    }
+}
